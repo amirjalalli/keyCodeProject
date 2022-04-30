5a3b5aeaaca546bc8c4c18eaf3e7c15633426742
amirjalalli <87828297+amirjalalli@users.noreply.github.com>
2022-04-30 13:19:42 +0430

    keyCodeProject

---
+++ js/script.js
@@ -0,0 +1,35 @@
+﻿let $ = document
+let locationKey = $.querySelector('#location')
+let title = $.querySelector('title')
+
+
+let keyCodeElem = $.querySelector("#keyCode")
+let keyElem = $.querySelector("#key")
+let locationElem = $.querySelector("#location")
+let whichElem = $.querySelector("#which")
+let codeElem = $.querySelector("#code")
+
+
+document.body.addEventListener('keydown', function (event) {
+	event.preventDefault();
+	starter.style.display = 'none'
+	heading.style.display = 'flex'
+	ascii.style.display = 'flex'
+	infos.style.display = 'flex'
+	
+	let eventkeyCode = event.keyCode
+	let eventKey = event.key;
+	let eventLocation = event.location;
+	let eventWhich = event.which;
+	let eventCode = event.code
+
+
+	keyCodeElem.innerHTML = eventkeyCode
+	keyElem.innerHTML = eventKey
+	locationElem.innerHTML = eventLocation;
+	whichElem.innerHTML = eventWhich;
+	codeElem.innerHTML = eventCode;
+
+
+	console.log(event)
+})
